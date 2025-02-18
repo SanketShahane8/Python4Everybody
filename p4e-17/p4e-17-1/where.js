f2cd328c7ff5d0516e58b9c81c4d44b02812a620
SanketShahane8 <sanket.shahane@outlook.com>
2025-02-18 10:21:53 +0530

    Completed exercise for getting geo location and showing on map

---
+++ p4e-17/p4e-17-1/where.js
@@ -0,0 +1,10 @@
+myData = [
+[18.9673399,72.8287291, 'Rolex restaurant'],
+[18.9668801,72.8284709, 'Cafe Sagar'],
+[18.9667686,72.8291706, 'Sarvi Restaurant'],
+[18.9788845,72.8337008, 'Maharaja Cafe'],
+[-8.5675,13.47639, 'Maharaja Cafe'],
+[18.9686154,72.8210412, 'Anna Leela Veg Restaurant'],
+[18.9177853,72.8292742, 'Shubh Sagar'],
+[19.4246224,73.4599357, 'Siddhu Family Dhaba']
+];
